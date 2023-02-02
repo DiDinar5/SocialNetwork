86feba86f37f9e84dcc2c0267f57f8db9e55d4d9
Dinar <dinar-wp9@mail.ru>
2023-02-02 16:40:18 +0300

    Add project files.

---
+++ BLL/Services/FriendService.cs
@@ -0,0 +1,57 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Services
+{
+    
+    public class FriendService
+    {
+        IUserRepository userRepository;
+        IFriendRepository friendRepository;
+        public FriendService()
+        {
+            userRepository = new UserRepository();
+            friendRepository = new FriendRepository();
+
+        }
+        //public IEnumerable<Friends> GetIncomingRequestByUserId(int recipientId)
+        //{
+        //    var friends = new List<Friends>();
+
+        //    friendRepository.FindAllByUserId(recipientId).ToList().ForEach(m =>
+        //    {
+        //        var senderUserEntity = userRepository.FindById(m.friend_id);
+
+        //        friends.Add(new Friends(m.id, m.friend_id, m.user_id));
+        //    });
+
+        //    return friends;
+        //}
+        public void AddFriend(Friends friends,FriendsEmail friendsEmail)
+        {
+            if (friends.Id == 0)
+                throw new UserNotFoundException();
+            if(friends.FriendId == 0)
+                throw new UserNotFoundException();
+            if(friends.UserId == 0)
+                throw new UserNotFoundException();
+            if (String.IsNullOrEmpty(friendsEmail.Email))
+                throw new UserNotFoundException();
+
+            var friendEntity = new FriendEntity()
+            {
+                id = friends.Id,
+                user_id = friends.UserId,
+                friend_id = friends.FriendId
+            };
+        }
+    }    
+    
+}
---
+++ PLL/Views/AddFriendsViews.cs
@@ -0,0 +1,41 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class AddFriendsViews
+    {
+        UserService userService;
+        FriendService friendService;
+        public AddFriendsViews(UserService userService,FriendService friendService)
+        {
+            this.friendService = friendService;
+            this.userService = userService;
+        }
+        public void Show(User user)
+        {
+            var friendsEmail = new FriendsEmail();
+            var friends = new Friends();
+            
+            Console.Write("Введите почтовый адрес друга: ");
+            friendsEmail.Email = Console.ReadLine();
+            try
+            {
+                friendService.AddFriend(friends,friendsEmail);
+                SuccessMessage.Show("Друг успешно найден!");;
+                user = userService.FindByEmail(user.Email);
+            }
+            catch(UserNotFoundException)
+            {
+                AlertMessage.Show("Пользователь не найден!");
+            }
+        }
+    }
+}
